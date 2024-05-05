ef779b5ecb232f23930b6c435fa37f06d6daf131
alcode111 <al.code.111@gmail.com>
2024-05-05 17:26:17 +0200

    Finish UI design of title screen with dummy data

--- SwiftUIHPTrivia/ContentView.swift
+++ SwiftUIHPTrivia/ContentView.swift
@@ -24,10 +24,74 @@ struct ContentView: View {
                         
                         Text("HP")
                             .font(.custom(Constants.hpFont, size: 70))
+                            .padding(.bottom, -50)
                         
                         Text("Trivia")
                             .font(.custom(Constants.hpFont, size: 60))
                     }
+                    .padding(.top, 70)
+                    
+                    Spacer()
+                    
+                    VStack {
+                        Text("Recent scores")
+                            .font(.title2)
+                        
+                        Text("33")
+                        Text("27")
+                        Text("15")
+                    }
+                    .font(.title3)
+                    .padding(.horizontal)
+                    .foregroundStyle(.white)
+                    .background(.black.opacity(0.7))
+                    .cornerRadius(17)
+                    
+                    Spacer()
+                    
+                    HStack {
+                        Spacer()
+                        
+                        Button {
+                            //Show game instructions
+                        } label: {
+                            Image(systemName: "info.circle.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            // Start new game
+                        } label: {
+                            Text("Play")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .padding(.vertical, 7)
+                                .padding(.horizontal, 50)
+                                .background(.brown)
+                                .cornerRadius(7)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "gearshape.fill")
+                                .font(.largeTitle)
+                                .foregroundStyle(.white)
+                                .shadow(radius: 5)
+                        }
+                        
+                        Spacer()
+                    }
+                    .frame(width: geo.size.width)
+                    
+                    Spacer()
                 }
             }
             .frame(width: geo.size.width, height: geo.size.height)
