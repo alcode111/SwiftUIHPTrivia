43f1e7a63e6e589bdc1c9d6fd02bd354e83d9e0e
alcode111 <al.code.111@gmail.com>
2024-05-05 11:53:38 +0200

    Initial Commit

---
+++ SwiftUIHPTrivia/SwiftUIHPTriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHPTriviaApp.swift
+//  SwiftUIHPTrivia
+//
+//  Created by Ismaïl on 05/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHPTriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
