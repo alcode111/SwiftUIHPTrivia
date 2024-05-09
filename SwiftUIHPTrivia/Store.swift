e18ea24b865c8981cf77fd3c6f975feca57d3569
alcode111 <al.code.111@gmail.com>
2024-05-09 19:14:20 +0200

    Create purchase function

--- SwiftUIHPTrivia/Store.swift
+++ SwiftUIHPTrivia/Store.swift
@@ -19,6 +19,7 @@ class Store: ObservableObject {
     @Published var books: [BookStatus] =
     [.active, .active, .inactive, .locked, .locked, .locked, .locked]
     @Published var products: [Product] = []
+    @Published var purchasedIDs = Set<String>()
     
     private var productsID = ["hp4", "hp5", "hp6", "hp7"]
     
@@ -27,6 +28,35 @@ class Store: ObservableObject {
             products = try await Product.products(for: productsID)
         } catch {
             print("Couldn't fetch those products: \(error)")
+        }
+    }
+    
+    func purchase(_ product: Product) async {
+        do {
+            let result = try await product.purchase()
+            
+            switch result {
+            // Purchase was sucessful, but now we have to verify the receipt
+            case .success(let verificationResult):
+                switch verificationResult {
+                case .unverified(let signedType, let verificationError):
+                    print("Error on \(signedType): \(verificationError)")
+                case .verified(let signedType):
+                    purchasedIDs.insert(signedType.productID)
+                }
+                
+            // User cancelled or parent disapproved child's purchase request
+            case .userCancelled:
+                break
+                
+            // Waiting for approval
+            case .pending:
+                break
+            @unknown default:
+                break
+            }
+        } catch {
+            print("Couldn't purchase that product: \(error)")
         }
     }
 }
